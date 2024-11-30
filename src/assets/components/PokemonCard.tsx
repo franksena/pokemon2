8d7d3c38372f762fdcad453eefce153751e5849f
Frank Sena AGBOLOSU <francksena@yahoo.com>
2024-11-30 22:47:06 +0100

    Props.1 de la quête 4

--- src/assets/components/PokemonCard.tsx
+++ src/assets/components/PokemonCard.tsx
@@ -1,9 +1,14 @@
-function PokemonCard() {
+
+
+
+function PokemonCard({pokemon}) {
     return ( 
         <div>
-            <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png " alt="Bulbasaur" />
-                <figcaption>Bulbasaur 1</figcaption>
+         <figure>
+         {pokemon.imgSrc ? (<img src= {pokemon.imgSrc} alt= {pokemon.name} />):(<p>???</p> )
+         }
+         <figcaption>{pokemon.name} </figcaption>  
+                
             </figure>
         </div>
      );
